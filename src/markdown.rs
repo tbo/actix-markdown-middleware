a6419494d3ca5909690e09840a5e868e8bc5e2da
Thomas Bonk <thomas@bonk.dev>
2020-04-14 00:10:29 +0200

    Add working markdown transformation

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,5 +1,4 @@
 use std::future::Future;
-use std::marker::PhantomData;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
@@ -9,6 +8,14 @@ use actix_web::body::{BodySize, MessageBody, ResponseBody};
 use actix_web::{dev::ServiceRequest, dev::ServiceResponse, Error};
 use bytes::{Bytes, BytesMut};
 use futures::future::{ok, Ready};
+use pulldown_cmark::{html, Options, Parser};
+
+pub enum ConditionalResponse<A, I> {
+    Active(A),
+    Inactive(I),
+}
+
+type MarkdownResponse<B> = ConditionalResponse<MarkdownBody<B>, ResponseBody<B>>;
 
 pub struct Transformer;
 
@@ -18,7 +25,7 @@ where
     B: MessageBody + 'static,
 {
     type Request = ServiceRequest;
-    type Response = ServiceResponse<BodyLogger<B>>;
+    type Response = ServiceResponse<MarkdownResponse<B>>;
     type Error = Error;
     type InitError = ();
     type Transform = MarkdownTransformerMiddleware<S>;
@@ -39,104 +46,120 @@ where
     B: MessageBody,
 {
     type Request = ServiceRequest;
-    type Response = ServiceResponse<BodyLogger<B>>;
+    type Response = ServiceResponse<MarkdownResponse<B>>;
     type Error = Error;
-    type Future = WrapperStream<S, B>;
+    type Future = WrapperStream<S>;
 
     fn poll_ready(&mut self, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
         self.service.poll_ready(cx)
     }
 
     fn call(&mut self, req: ServiceRequest) -> Self::Future {
-        // type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
-        // Box::pin(async move {
-        //     let res = fut.await?;
-        //     if let Some(test) = &res
-        //         .headers()
-        //         .get("content-type")
-        //         .map(|header| header.eq(&HeaderValue::from_static("text/markdown")))
-        //     {
-        //         dbg!(test);
-        //     }
-        //     Ok(res)
-        // })
         WrapperStream {
             fut: self.service.call(req),
-            _t: PhantomData,
         }
     }
 }
 
+fn get_buffer_with_capacity(capacity: BodySize) -> Vec<u8> {
+    use BodySize::*;
+    match capacity {
+        Sized(capacity) => Vec::with_capacity(capacity),
+        Sized64(capacity) => Vec::with_capacity(capacity as usize),
+        _ => Vec::new(),
+    }
+}
+
 #[pin_project::pin_project]
-pub struct WrapperStream<S, B>
+pub struct WrapperStream<S>
 where
-    B: MessageBody,
     S: Service,
 {
     #[pin]
     fut: S::Future,
-    _t: PhantomData<(B,)>,
 }
 
-impl<S, B> Future for WrapperStream<S, B>
+impl<S, B> Future for WrapperStream<S>
 where
     B: MessageBody,
     S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
 {
-    type Output = Result<ServiceResponse<BodyLogger<B>>, Error>;
+    type Output = Result<ServiceResponse<MarkdownResponse<B>>, Error>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let res = futures::ready!(self.project().fut.poll(cx));
 
         Poll::Ready(res.map(|res| {
-            if let Some(test) = &res
+            if res
                 .headers()
                 .get("content-type")
                 .map(|header| header.eq(&HeaderValue::from_static("text/markdown")))
+                .unwrap_or(false)
             {
-                dbg!(test);
+                dbg!(&res);
                 return res.map_body(move |_, body| {
-                    ResponseBody::Body(BodyLogger {
+                    let size = body.size();
+                    ResponseBody::Body(MarkdownResponse::Active(MarkdownBody {
                         body,
                         body_accum: BytesMut::new(),
-                    })
+                        buffer: get_buffer_with_capacity(size),
+                    }))
                 });
             }
-            res.map_body(move |_, body| {
-                ResponseBody::Body(BodyLogger {
-                    body,
-                    body_accum: BytesMut::new(),
-                })
-            })
+            res.map_body(move |_, body| ResponseBody::Body(MarkdownResponse::Inactive(body)))
         }))
     }
 }
 
-pub struct BodyLogger<B> {
+pub struct MarkdownBody<B> {
     body: ResponseBody<B>,
     body_accum: BytesMut,
+    buffer: Vec<u8>,
 }
 
-impl<B> Drop for BodyLogger<B> {
-    fn drop(&mut self) {
-        println!("response body: {:?}", self.body_accum);
+impl<B: MessageBody> MarkdownBody<B> {
+    fn is_complete(&self) -> bool {
+        use BodySize::*;
+        match self.body.size() {
+            None | Empty => true,
+            Sized(size) => size <= self.buffer.len(),
+            Sized64(size) => size <= self.buffer.len() as u64,
+            _ => false,
+        }
     }
 }
 
-impl<B: MessageBody> MessageBody for BodyLogger<B> {
+impl<B: MessageBody> MessageBody for MarkdownResponse<B> {
     fn size(&self) -> BodySize {
-        self.body.size()
+        use ConditionalResponse::*;
+        match self {
+            Active(_body) => BodySize::Stream,
+            Inactive(body) => body.size(),
+        }
     }
 
     fn poll_next(&mut self, cx: &mut Context<'_>) -> Poll<Option<Result<Bytes, Error>>> {
-        match self.body.poll_next(cx) {
-            Poll::Ready(Some(Ok(chunk))) => {
-                self.body_accum.extend_from_slice(&chunk);
-                Poll::Ready(Some(Ok(chunk)))
-            }
-            Poll::Ready(Some(Err(e))) => Poll::Ready(Some(Err(e))),
-            Poll::Ready(None) => Poll::Ready(None),
-            Poll::Pending => Poll::Pending,
+        use ConditionalResponse::*;
+        match self {
+            Inactive(body) => body.poll_next(cx),
+            Active(body) => match body.body.poll_next(cx) {
+                Poll::Ready(Some(Ok(chunk))) => {
+                    body.body_accum.extend_from_slice(&chunk);
+                    body.buffer.extend_from_slice(&chunk);
+                    if !body.is_complete() {
+                        return Poll::Pending;
+                    }
+                    let s = &String::from_utf8_lossy(&body.buffer);
+                    let parser = Parser::new_ext(s, Options::empty());
+                    let mut html_output: String = String::with_capacity(s.len() * 3 / 2);
+                    html::push_html(&mut html_output, parser);
+                    dbg!(&html_output);
+                    Poll::Ready(Some(Ok(Bytes::from(html_output))))
+                }
+                Poll::Ready(Some(Err(e))) => Poll::Ready(Some(Err(e))),
+                Poll::Ready(None) => Poll::Ready(None),
+                Poll::Pending => Poll::Pending,
+            },
         }
     }
 }
